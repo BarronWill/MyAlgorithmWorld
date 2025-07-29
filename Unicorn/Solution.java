107cd40d7a8b87c21208577d2b53e0c0420ad3d5
BarronWill <viet22082003@gmail.com>
2025-07-30 00:09:39 +0700

    upload file

--- Unicorn/Solution.java
+++ Unicorn/Solution.java
@@ -21,41 +21,41 @@ public class Solution {
             this.count = count;
         }
     }
-public static int run (int x, int y) {
-    q.offer(new Point(x, y, 0, 0, 0, 3));
+    public static int run (int x, int y) {
+        q.offer(new Point(x, y, 0, 0, 0, 3));
 
-    while (!q.isEmpty()) {
-        Point p = q.poll();
-        if (p.x <= 0 || p.y <= 0 || p.x > N || p.y > N) continue;
-        
-        if (p.x == U && p.y == L) {
-            return p.knightStep - p.unicornStep;
-        }
-
-        if (checked[p.x][p.y][p.way] != 0 && checked[p.x][p.y][p.way] <= p.unicornStep)  continue;
-
-        checked[p.x][p.y][p.way] = p.unicornStep;
-
-        for (int i = 1; i <= 8; i++) {
-            int delay = 1;
-            int count = 3;
-
-            if (p.way == i && p.count > 1) {
-                delay = 0;
-                count = p.count - 1;
+        while (!q.isEmpty()) {
+            Point p = q.poll();
+            if (p.x <= 0 || p.y <= 0 || p.x > N || p.y > N) continue;
+            
+            if (p.x == U && p.y == L) {
+                return p.knightStep - p.unicornStep;
             }
 
-            q.offer(new Point(
-                p.x + dx[i], p.y + dy[i], i,
-                p.knightStep + 1,
-                p.unicornStep + delay,
-                count
-            ));
-        }
-    }
+            if (checked[p.x][p.y][p.way] != 0 && checked[p.x][p.y][p.way] <= p.unicornStep)  continue;
 
-    return -1;
-}
+            checked[p.x][p.y][p.way] = p.unicornStep;
+
+            for (int i = 1; i <= 8; i++) {
+                int delay = 1;
+                int count = 3;
+
+                if (p.way == i && p.count > 1) {
+                    delay = 0;
+                    count = p.count - 1;
+                }
+
+                q.offer(new Point(
+                    p.x + dx[i], p.y + dy[i], i,
+                    p.knightStep + 1,
+                    p.unicornStep + delay,
+                    count
+                ));
+            }
+        }
+
+        return -1;
+    }
     public static void main(String[] args) throws IOException {
         FileInputStream file = new FileInputStream("input.txt");
         BufferedReader reader = new BufferedReader(new InputStreamReader(file));
